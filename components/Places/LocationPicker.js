fb594b6bcbe77b849c240c5d409afc3cd07ff2bb
officialcc <xnnproject@yahoo.co.uk>
2026-03-22 21:46:31 +0800

    Completed Lesson 203 + Implemented .env

--- components/Places/LocationPicker.js
+++ components/Places/LocationPicker.js
@@ -1,10 +1,14 @@
-import { Alert, StyleSheet, View } from "react-native";
+import { useState } from "react";
+import { Alert, Image, StyleSheet, View, Text } from "react-native";
 import { getCurrentPositionAsync, useForegroundPermissions, PermissionStatus } from "expo-location";
 
 import OutlinedButton from "../UI/OutlinedButton";
 import { Colors } from "../../constants/colors";
+import { getMapPreview } from "../../util/location";
 
 function LocationPicker() {
+    const [pickedLocation, setPickedLocation] = useState();
+
     const [locationPermissionInformation, requestPermission] = useForegroundPermissions();
 
     async function verifyPermissions() {
@@ -33,14 +37,30 @@ function LocationPicker() {
         }
 
         const location = await getCurrentPositionAsync({accuracy: 6});
-        console.log(location);
+        setPickedLocation({
+            lat: location.coords.latitude,
+            lng: location.coords.longitude,
+        });
     }
 
     function pickOnMapHandler() {}
 
+    let locationPreview = <Text>No location picked yet.</Text>
+
+    if (pickedLocation) {
+        locationPreview = (
+            <Image
+                style={styles.image}
+                source={{
+                    uri: getMapPreview(pickedLocation.lat, pickedLocation.lng),
+                }}
+            />
+        )
+    }
+
     return (
         <View>
-            <View style={styles.mapPreview}></View>
+            <View style={styles.mapPreview}>{locationPreview}</View>
             <View style={styles.actions}>
                 <OutlinedButton icon="location" onPress={getLocationHandler}>Locate User</OutlinedButton>
                 <OutlinedButton icon="map" onPress={pickOnMapHandler}>Pick On Map</OutlinedButton>
@@ -60,10 +80,16 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: Colors.primary100,
         borderRadius: 4,
+        overflow: 'hidden',
     },
     actions: {
         flexDirection: 'row',
         justifyContent: 'space-around',
         alignItems: 'center',
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+        // borderRadius: 4,
     },
 });
---
+++ util/location.js
@@ -0,0 +1,7 @@
+import { GOOGLE_API_KEY } from '@env';
+// const GOOGLE_API_KEY = 'AIzaSyBDkpL49MhIP4em4ppukD_zyUJfyJJuZss';
+
+export function getMapPreview(lat, lng) {
+    const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
+    return imagePreviewUrl;
+}
